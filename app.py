9596139c94320467fc9937fcb819e8bcbec2d6a2
DivyanshKaran <karandivyansh01@gmail.com>
2024-09-10 12:58:19 +0530

    Second Session

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -48,3 +49,39 @@ async def query(height: int=180,weight: int=80,name: str="divyansh", grade: str 
 @app.get("/{id}/items/{name}")
 async def read(id:int ,name: str,grade: str | None = None):
     return {"message": f"{name} with student {id}"}
+
+# how to send a Request body
+
+class Item(BaseModel):
+    name : str
+    description: str | None = None
+    price: int
+
+@app.post("/item")
+async def getItem(item: Item):
+    # print(item)
+    # print(item.dict())
+    return item
+
+# Additional Validation of query parameteres
+from fastapi import Query
+from typing import Annotated
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None , Query(min_length=3,max_length=50,pattern="^fixedQuery$")] = "..."):
+    results = {}
+    if q:
+         results.update({"q":q})
+    return results
+
+@app.post("/items/")
+async def read_item(q: Annotated[list[str]| None,Query(title="Database Call",description="A query to send parameters of the data base call",alias="database-query",deprecated=True,include_in_schema=False)]=["divyansh","karan"]):
+    query = {"q":q}
+    return query
+
+from fastapi import Path
+@app.get("/items/{item_id}/")
+async def senditem(item_id: Annotated[int,Path(description="The id of the item to get",ge=10)]):
+    return {"message": f"The Item id is {item_id}"}
+# To FastAPI it dosent't matter if you declare default values after non default values
+# You'd never have problems if you Annnoted
